aa9640828fb0bf6eda3df2fa34114680bf82f05c
Radmir Musabaev <musabaevradmir@gmail.com>
2024-05-30 19:59:40 +0400

    using bcrypt instead of rsa

--- src/main/java/com/astu/ibolympapi/config/ApplicationConfiguration.java
+++ src/main/java/com/astu/ibolympapi/config/ApplicationConfiguration.java
@@ -1,7 +1,6 @@
 package com.astu.ibolympapi.config;
 
 import com.astu.ibolympapi.user.repositories.UserRepo;
-import com.auth0.jwt.algorithms.Algorithm;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,14 +10,8 @@ import org.springframework.security.authentication.dao.DaoAuthenticationProvider
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-
-import java.security.KeyPair;
-import java.security.KeyPairGenerator;
-import java.security.NoSuchAlgorithmException;
-import java.security.interfaces.RSAPrivateKey;
-import java.security.interfaces.RSAPublicKey;
 
 @Configuration
 @RequiredArgsConstructor
@@ -46,23 +39,6 @@ public class ApplicationConfiguration {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
-    }
-
-    @Bean
-    public KeyPairGenerator keyPairGenerator() throws NoSuchAlgorithmException {
-        KeyPairGenerator keyGenerator = KeyPairGenerator.getInstance("RSA");
-        keyGenerator.initialize(2048);
-        return keyGenerator;
-    }
-
-    @Bean
-    public KeyPair keyPair() throws NoSuchAlgorithmException {
-        return keyPairGenerator().generateKeyPair();
-    }
-
-    @Bean
-    public Algorithm signInAlgorithm() throws NoSuchAlgorithmException {
-        return Algorithm.RSA256((RSAPublicKey) keyPair().getPublic(), (RSAPrivateKey) keyPair().getPrivate());
+        return new BCryptPasswordEncoder();
     }
 }
