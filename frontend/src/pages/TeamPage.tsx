0093412cda031ec5c0fdfa0b8c486dcaf815f254
Radmir Musabaev <musabaevradmir@gmail.com>
2024-06-13 18:43:45 +0400

    feat: created team page

---
+++ frontend/src/pages/TeamPage.tsx
@@ -0,0 +1,11 @@
+import { TeamForm } from "@/modules/Team"
+
+const TeamPage = () => {
+    return (
+        <main className="flex flex-col items-center bg-my-white min-h-[91vh]">
+            <TeamForm />
+        </main>
+    )
+}
+
+export default TeamPage
