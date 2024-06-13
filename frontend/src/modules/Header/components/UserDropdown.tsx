c7c138c545576d4596a3cf7896bbc6005eb450f7
Radmir Musabaev <musabaevradmir@gmail.com>
2024-06-13 18:43:20 +0400

    feat: added button for team page

--- frontend/src/modules/Header/components/UserDropdown.tsx
+++ frontend/src/modules/Header/components/UserDropdown.tsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 import { FC } from "react"
 
 interface UserDropDownProps {
@@ -7,6 +7,7 @@ interface UserDropDownProps {
 }
 
 const UserDropdown: FC<UserDropDownProps> = ({userName, logout}) => {
+    const navigate = useNavigate()
     return (
         <div
             aria-label="user-dropdown-menu"
@@ -17,19 +18,31 @@ const UserDropdown: FC<UserDropDownProps> = ({userName, logout}) => {
                 className="flex items-center justify-center w-40 h-20 transition-all duration-500 group-hover:bg-my-blue"
             >
                 <b className="text-xl font-normal font-russo text-my-blue group-hover:text-my-dark">
-                    {userName}
+                    Профиль
                 </b>
             </Link>
 
-            <button
-                onClick={logout}
-                type="button"
-                className="absolute z-10 hidden w-40 bg-my-dark bg-my-gray rounded-xl origin-top-center group-hover:block group-hover:animate-append active:scale-99"
-            >
-                <b className="text-xl font-normal font-russo text-my-white hover:text-my-blue">
-                    Выйти
-                </b>
-            </button>
+            <div className="absolute gap-5 z-10 hidden w-40 bg-my-dark bg-my-gray rounded-xl origin-top-center group-hover:block group-hover:animate-append active:scale-99">
+                <button
+                    onClick={() => navigate("/team")}
+                    type="button"
+
+                >
+                    <b className="text-xl font-normal font-russo text-my-white hover:text-my-blue">
+                        Моя команда
+                    </b>
+                </button>
+
+                <button
+                    onClick={logout}
+                    type="button"
+                >
+                    <b className="text-xl font-normal font-russo text-my-white hover:text-my-blue">
+                        Выйти
+                    </b>
+                </button>
+            </div>
+
         </div>
     )
 }
