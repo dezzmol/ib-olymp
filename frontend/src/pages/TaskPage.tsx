951cd77288da27cbf9ff1056c87a578265dac3fe
Radmir Musabaev <musabaevradmir@gmail.com>
2024-06-17 11:13:14 +0400

    feat: created task page

---
+++ frontend/src/pages/TaskPage.tsx
@@ -0,0 +1,11 @@
+import { TaskForm } from "@/modules/Task"
+
+const TaskPage = () => {
+    return (
+        <main className="flex flex-col items-center min-h-[91vh] bg-my-white">
+            <TaskForm />
+        </main>
+    )
+}
+
+export default TaskPage
