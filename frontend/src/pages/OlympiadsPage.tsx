ee586b2bee9e9c8f74ce8c78cf646bd2150c5cd8
Radmir Musabaev <musabaevradmir@gmail.com>
2024-06-17 22:45:46 +0400

    feat: created olympiads page

---
+++ frontend/src/pages/OlympiadsPage.tsx
@@ -0,0 +1,11 @@
+import { OlympiadsForm } from "@/modules/Olympiads"
+
+const OlympiadsPage = () => {
+    return (
+        <main className="flex flex-col items-center bg-my-white min-h-[91vh]">
+            <OlympiadsForm />
+        </main>
+    )
+}
+
+export default OlympiadsPage
