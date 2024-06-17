97d5fe4df4c238a2aeb2c6d7ffc355dd197ac98b
Radmir Musabaev <musabaevradmir@gmail.com>
2024-06-17 11:15:08 +0400

    feat: added new paths

--- frontend/src/modules/AppRouter/routes/index.ts
+++ frontend/src/modules/AppRouter/routes/index.ts
@@ -3,13 +3,15 @@ import MainPage from "@/pages/MainPage.tsx"
 import {
     ADMIN_PAGE_ROUTE,
     LOGIN_ROUTE,
-    MAIN_PAGE_ROUTE,
+    MAIN_PAGE_ROUTE, OLYMPIAD_ADMIN_ROUTE,
     PROFILE_ROUTE, TEAM_ROUTE
 } from "@/modules/AppRouter/utils/consts.ts"
 import LoginPage from "@/pages/LoginPage.tsx"
 import AdminPage from "@/pages/AdminPage.tsx"
 import ProfilePage from "@/pages/ProfilePage.tsx"
 import TeamPage from "@/pages/TeamPage.tsx"
+import OlympiadAdminPage from "@/pages/OlympiadAdminPage.tsx"
+import TaskPage from "@/pages/TaskPage.tsx"
 
 interface IPages {
     path: string
@@ -24,5 +26,7 @@ export const publicPages: IPages[] = [
 export const privatePages: IPages[] = [
     { path: ADMIN_PAGE_ROUTE, component: AdminPage },
     { path: PROFILE_ROUTE, component: ProfilePage },
-    { path: TEAM_ROUTE, component: TeamPage }
+    { path: TEAM_ROUTE, component: TeamPage },
+    { path: OLYMPIAD_ADMIN_ROUTE, component: OlympiadAdminPage },
+    { path: ADMIN_PAGE_ROUTE + "/tasks/:id", component: TaskPage }
 ]
