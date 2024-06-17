6caa5c9aaafa223530993ac9c624d8061fdc847a
Radmir Musabaev <musabaevradmir@gmail.com>
2024-06-17 11:14:19 +0400

    feat: created olymp admin page

---
+++ frontend/src/pages/OlympiadAdminPage.tsx
@@ -0,0 +1,11 @@
+import OlympiadAdminForm from "@/modules/OlympiadAdmin/components/OlympiadAdminForm.tsx"
+
+const OlympiadAdminPage = () => {
+    return (
+        <main className="flex flex-col items-center min-h-[91vh] bg-my-white">
+            <OlympiadAdminForm />
+        </main>
+    )
+}
+
+export default OlympiadAdminPage
