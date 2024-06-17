b0d4fac695ce48380adc4e3bf8426b98fabf38e3
Radmir Musabaev <musabaevradmir@gmail.com>
2024-06-17 11:15:27 +0400

    feat: added styles for profile page

--- frontend/src/pages/ProfilePage.tsx
+++ frontend/src/pages/ProfilePage.tsx
@@ -3,7 +3,7 @@ import StudentForm from "@/modules/Profile/components/StudentForm.tsx"
 
 const ProfilePage = () => {
     return (
-        <main className="flex flex-col items-center bg-my-white min-h-[91vh]">
+        <main className="flex flex-col items-center bg-my-white min-h-[91vh] max-w-[1150px]">
             <ProfileForm />
             <StudentForm />
         </main>
