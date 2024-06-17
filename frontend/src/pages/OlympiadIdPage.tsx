5a62c5c884ef6cc3ed3c9c015cb36e3c8e0302ff
Radmir Musabaev <musabaevradmir@gmail.com>
2024-06-17 22:45:25 +0400

    feat: created olympiad id page

---
+++ frontend/src/pages/OlympiadIdPage.tsx
@@ -0,0 +1,11 @@
+import { OlympiadIdForm } from "@/modules/OlympiadId"
+
+const OlympiadIdPage = () => {
+    return (
+        <main className="flex flex-col items-center bg-my-white min-h-[91vh]">
+            <OlympiadIdForm />
+        </main>
+    )
+}
+
+export default OlympiadIdPage
