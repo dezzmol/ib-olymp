780b6d8425b6728317a97367e8276a1ecaecc399
Radmir Musabaev <musabaevradmir@gmail.com>
2024-06-23 13:35:51 +0400

    feat: created new endpoints

--- src/main/java/com/astu/ibolympapi/olympiads/controller/OlympiadController.java
+++ src/main/java/com/astu/ibolympapi/olympiads/controller/OlympiadController.java
@@ -1,12 +1,17 @@
 package com.astu.ibolympapi.olympiads.controller;
 
+import com.astu.ibolympapi.olympiads.dto.IsTaskOpenDTO;
 import com.astu.ibolympapi.olympiads.dto.OlympiadDTO;
 import com.astu.ibolympapi.olympiads.service.OlympiadService;
+import com.astu.ibolympapi.tasks.dto.SolutionDTO;
 import com.astu.ibolympapi.tasks.dto.TaskDTO;
-import com.astu.ibolympapi.tasks.entities.Task;
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
@@ -26,6 +31,17 @@ public class OlympiadController {
         return ResponseEntity.ok(service.getAllOlympiadDTO());
     }
 
+    @GetMapping("/{olymp_id}/tasks/{task_id}/attachments/{fileName:.+}")
+    public ResponseEntity<Resource> getOlympiadAttachment(@PathVariable Long olymp_id, @PathVariable Long task_id, @PathVariable String fileName) {
+        Resource resource = service.getAttachment(olymp_id, task_id, fileName);
+        String contentType = "application/octet-stream";
+        String headerValue = "attachment; filename=\"" + resource.getFilename() + "\"";
+        return ResponseEntity.ok()
+                .contentType(MediaType.parseMediaType(contentType))
+                .header(HttpHeaders.CONTENT_DISPOSITION, headerValue)
+                .body(resource);
+    }
+
     @PostMapping("/{olympiad_id}/registration")
     public ResponseEntity<String> registerOnOlympiad(@PathVariable Long olympiad_id) {
         service.registrationOnOlympiad(olympiad_id);
@@ -40,6 +56,33 @@ public class OlympiadController {
     @GetMapping("/{olympiad_id}/tasks/{task_id}")
     public ResponseEntity<TaskDTO> getOlympiadTask(@PathVariable Long olympiad_id, @PathVariable Long task_id) {
         return ResponseEntity.ok(service.getOlympiadTask(olympiad_id, task_id));
+    }
+
+    @GetMapping("/{olympiad_id}/tasks/{task_id}/check")
+    public ResponseEntity<IsTaskOpenDTO> checkIsTaskOpened(@PathVariable Long olympiad_id, @PathVariable Long task_id) {
+        return ResponseEntity.ok(service.checkIsTaskOpened(olympiad_id, task_id));
+    }
+
+    @PostMapping("/{olympiad_id}/tasks/{task_id}/open")
+    public ResponseEntity<String> openTask(@PathVariable Long olympiad_id, @PathVariable Long task_id) {
+        service.openTask(olympiad_id, task_id);
+        return ResponseEntity.ok("Task opened");
+    }
+
+    @PostMapping("/{olympiad_id}/tasks/{task_id}/uploadSolution")
+    public ResponseEntity<String> uploadSolutionWithFile(
+            @RequestParam(value = "file",required = false) MultipartFile file,
+            @PathVariable Long olympiad_id,
+            @PathVariable Long task_id
+    ) {
+        service.uploadSolutionWithFile(file, olympiad_id, task_id);
+        return ResponseEntity.ok("Solution with file accepted");
+    }
+
+    @PostMapping("/{olympiad_id}/tasks/{task_id}/solution")
+    public ResponseEntity<String> uploadSolution(@PathVariable Long olympiad_id, @PathVariable Long task_id, @RequestBody SolutionDTO solutionDTO) {
+        service.uploadSolution(olympiad_id, task_id, solutionDTO);
+        return ResponseEntity.ok("Solution accepted");
     }
 
     @GetMapping("/myolympiads")
