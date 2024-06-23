50873a3796e09ca0418d812581d1b19dfb23b321
Radmir Musabaev <musabaevradmir@gmail.com>
2024-06-23 13:34:56 +0400

    feat: created repository for answers

---
+++ src/main/java/com/astu/ibolympapi/olympiads/repositories/AnswerRepo.java
@@ -0,0 +1,15 @@
+package com.astu.ibolympapi.olympiads.repositories;
+
+import com.astu.ibolympapi.olympiads.entities.Answer;
+import com.astu.ibolympapi.olympiads.entities.Olympiad;
+import com.astu.ibolympapi.tasks.entities.Task;
+import com.astu.ibolympapi.team.entity.Team;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface AnswerRepo extends JpaRepository<Answer, Long> {
+    Optional<Answer> findAnswerByOlympiadAndTaskAndTeam(Olympiad olympiad, Task task, Team team);
+    Optional<List<Answer>> findAnswerByOlympiad(Olympiad olympiad);
+}
