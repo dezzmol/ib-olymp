3b73ac1165dd0d0f33f7b775dd1a8b1da781ee58
Radmir Musabaev <musabaevradmir@gmail.com>
2024-06-24 16:47:00 +0400

    feat: configure import from module

---
+++ frontend/src/modules/Estimate/index.ts
@@ -0,0 +1,9 @@
+import EstimateForm from "./components/EstimateForm.tsx"
+import EstimateOlympiadIdForm from "@/modules/Estimate/components/EstimateOlympiadIdForm.tsx"
+import EstimateTaskIdForm from "@/modules/Estimate/components/EstimateTaskIdForm.tsx"
+
+export {
+    EstimateForm,
+    EstimateOlympiadIdForm,
+    EstimateTaskIdForm
+}
